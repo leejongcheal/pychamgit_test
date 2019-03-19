05a05f893ba3d34763e21ca4c598ce8b1b3ad2ab
aksndk123 <aksndk123@naver.com>
2019-03-19 20:37:56 +0900

    adding second on master

--- main.py
+++ main.py
@@ -3,11 +3,14 @@
 #
 print("Hello World")
 
-#adding new item in Hotfix
+# adding new item in Hotfix
 print("Hello World2")
 
 # adding new item in master
 print("Hello World3")
 
 # adding new item in Hotfix
-print("Hello Wordl4")
+print("Hello World4")
+
+# adding seconde item in master
+print("Hello World5")
